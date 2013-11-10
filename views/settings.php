493c9684d20de5755689142f2e7d2fc1055b4e1a
n7studios <n7studios@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-10 16:33:31 +0000

    Version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/plugin-download-count/trunk@802053 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ views/settings.php
@@ -0,0 +1,92 @@
+<div class="wrap">
+    <div id="<?php echo $this->plugin->name; ?>-title" class="icon32"></div> 
+    <h2 class="wpcube"><?php echo $this->plugin->displayName; ?> &raquo; <?php _e('Settings'); ?></h2>
+           
+    <?php    
+    if (isset($this->message)) {
+        ?>
+        <div class="updated fade"><p><?php echo $this->message; ?></p></div>  
+        <?php
+    }
+    if (isset($this->errorMessage)) {
+        ?>
+        <div class="error fade"><p><?php echo $this->errorMessage; ?></p></div>  
+        <?php
+    }
+    ?> 
+    
+    <div id="poststuff">
+    	<div id="post-body" class="metabox-holder columns-2">
+    		<!-- Content -->
+    		<div id="post-body-content">
+    		
+    			<!-- Form Start -->
+		        <form id="post" name="post" method="post" action="admin.php?page=<?php echo $this->plugin->name; ?>">
+		            <div id="normal-sortables" class="meta-box-sortables ui-sortable">                        
+		                <div class="postbox">
+		                    <h3 class="hndle"><?php _e('Title', $this->plugin->name); ?></h3>
+		                    
+		                    <div class="option">
+		                    	<p>
+		                    		<strong><?php _e('Plugins', $this->plugin->name); ?></strong>
+		                    		<textarea name="<?php echo $this->plugin->name; ?>[plugins]"><?php echo (isset($this->settings['plugins']) ? $this->settings['plugins'] : ''); ?></textarea>
+		                    	</p>
+		                    	<p class="description">
+		                    		<?php _e('Enter the programmatic name of each plugin, one per line. For example, this plugin\'s programmatic name is plugin-download-count. Each Plugin listed here will form the download count total.', $this->plugin->name); ?>
+		                    	</p>
+		                    </div>
+		                    
+		                    <div class="option">
+		                    	<p>
+		                    		<strong><?php _e('Update Interval', $this->plugin->name); ?></strong>
+		                    		<input type="number" name="<?php echo $this->plugin->name; ?>[interval]" value="<?php echo (isset($this->settings['interval']) ? $this->settings['interval'] : '30'); ?>" min="15" max="9999" step="1" />
+		                    	</p>
+		                    	<p class="description">
+		                    		<?php _e('When a visitor views the download count, it\'s updated via AJAX periodically by querying the wordpress.org API. Define the number of seconds to check for a new download count.', $this->plugin->name); ?>
+		                    	</p>
+		                    </div>
+		                    
+		                    <div class="option">
+		                    	<p>
+		                       		<input type="submit" name="submit" value="<?php _e('Save', $this->plugin->name); ?>" class="button button-primary" /> 
+		                 		</p>
+		                    </div>
+		                </div>
+		                <!-- /postbox -->
+		                
+		                <div class="postbox">
+		                    <h3 class="hndle"><?php _e('Output', $this->plugin->name); ?></h3>
+		                    
+		                    <div class="option">
+		                    	<p>
+		                    		<?php _e('Use the shortcode [PDC] to display the download count on your Page, Post or Custom Post Type', $this->plugin->name); ?>
+		                    	</p>
+		                    </div>
+		                </div>
+		                
+		               
+					</div>
+					<!-- /normal-sortables -->
+			    </form>
+			    <!-- /form end -->
+    			
+    		</div>
+    		<!-- /post-body-content -->
+    		
+    		<!-- Sidebar -->
+    		<div id="postbox-container-1" class="postbox-container">
+    			<?php require_once($this->plugin->folder.'/_modules/dashboard/views/sidebar-donate.php'); ?>		
+    		</div>
+    		<!-- /postbox-container -->
+    	</div>
+	</div> 
+	
+	<!-- If this plugin has a pro/premium version, include this + change sidebar-donate = sidebar-upgrade -->
+	<div id="poststuff">
+    	<div id="post-body" class="metabox-holder columns-1">
+    		<div id="post-body-content">
+    			<?php require_once($this->plugin->folder.'/_modules/dashboard/views/footer-upgrade.php'); ?>
+    		</div>
+    	</div>
+    </div>        
+</div>
